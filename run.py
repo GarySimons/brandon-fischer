88878ec8090b3c50b9459d092fd3a1c7c608cb8e
GarySimons <garycsimons@gmail.com>
2021-03-04 07:11:25 +0000

    Added style.css. Created components, layout and pages folders in templates for html files

--- run.py
+++ run.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("pages/index.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("pages/about.html")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("pages/contact.html")
 
 
 if __name__ == "__main__":
