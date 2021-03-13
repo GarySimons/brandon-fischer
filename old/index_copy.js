62f264749c8402ba5451cda30c724488f9f25c9f
GarySimons <garycsimons@gmail.com>
2021-03-13 17:06:59 +0000

    Added Bootstrap slider to Interiors and Video pages

---
+++ old/index_copy.js
@@ -0,0 +1,24 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+// select all images
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+// Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+// Counter
+// select a slide
+let counter = 1;
+// Tell it width of image so knows how much to slide
+const size = carouselImages[0].clientWidth;
+
+// create slide that starts on image 1 rather than clone and moves on the x the size of image
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// Button listeners
+nextBtn.addEventListener('click',()=>{
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    // adds 1 to counter
+    counter++;
+    console.log(carouselSlide);
+});
