9c5e4080b95fcd9c35c007e845e63c8afdd33def
GarySimons <garycsimons@gmail.com>
2021-03-15 07:15:11 +0000

    Added html, css and js to Interiors page. Next button wired up to JS to move slider

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,24 @@
-$(document).ready(function () {
+const carouselSlide = document.querySelector('.carousel-slide');
+// select all images
+const carouselImages = document.querySelectorAll('.carousel-slide img');
 
+// Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+// Counter
+// select a slide
+let counter = 1;
+// Tell it width of image so knows how much to slide
+const size = carouselImages[0].clientWidth;
+
+// create slide that starts on image 1 rather than clone and moves on the x the size of image
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// Button listeners
+nextBtn.addEventListener('click',()=> {
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    // adds 1 to counter
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
